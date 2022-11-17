a284c2192fbf54c2a10a4df65f6ecb3ff0c29089
StivenSilva28 <desarrollo2@simde.com.co>
2022-11-17 14:12:45 -0500

    primer commit

---
+++ database/migrations/2022_11_17_024723_libros.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Libros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->engine = 'InnoDB';
+            $table->bigInteger('categoria_id')->unsigned();
+            $table->string('nombre');
+            $table->string('editorial');
+            $table->timestamps();
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
